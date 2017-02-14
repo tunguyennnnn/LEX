c374c741b9b79cccd9ca80fe5773410d7f6c1323
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2017-02-13 22:55:49 -0500

    Moduler prototype fetching logic
    This gets the video and transforms it to .opus
    Next step is to do .opus -> .ogg conversion with avconv/ffmpeg

---
+++ prototype/fetcher/logic.py
@@ -0,0 +1,27 @@
+import os
+import sys
+from youtube_dl import YoutubeDL
+
+
+def download_video(video):
+    def test_hook(d):
+        if d['status'] == 'finished':
+            print("Done downloading. Starting conversion")
+    # end test_hook
+
+    options = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'opus',
+        }],
+        'progress_hooks': [test_hook],
+        # 'writeinfojson': 'true',
+    }
+
+    downloader = YoutubeDL(options)
+    downloader.download([video])
+# end download_videos
+
+if __name__ == "__main__":
+    download_video(sys.argv[1:][0])
---
+++ prototype/testFetcher.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python3 fetcher/logic.py https://www.youtube.com/watch?v=dQw4w9WgXcQ
