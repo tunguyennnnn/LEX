b746464c642d65d8d4632fa6df134e22a16177e2
Samuel Masuy <smasuy@jive.com>
2017-02-15 01:16:36 -0500

    [infra] added transcriber Deployment and Service

--- infra/create-all-pods.sh
+++ infra/create-all-pods.sh
@@ -1,4 +1,5 @@
 kubectl create -f mongo.yaml
+kubectl create -f transcriber.yaml
 kubectl create -f apigateway.yaml
 kubectl create -f webclient.yaml
 
--- infra/delete-all-pods.sh
+++ infra/delete-all-pods.sh
@@ -1,4 +1,5 @@
 kubectl delete -f mongo.yaml
+kubectl delete -f transcriber.yaml
 kubectl delete -f apigateway.yaml
 kubectl delete -f webclient.yaml
 
