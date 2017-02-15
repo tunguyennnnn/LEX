1fd0ab8e21ee2abfe64751dc97f3305bb02afc16
Samuel Masuy <smasuy@jive.com>
2017-02-15 00:32:13 -0500

    [apigateway] configuration module

---
+++ apigateway/app/config/env/development.js
@@ -0,0 +1,8 @@
+const devConfig = {
+  env: 'development',
+  MONGOOSE_DEBUG: true,
+  db: 'mongodb://localhost/api_server',
+  port: 4000
+}
+
+module.exports = devConfig
---
+++ apigateway/app/config/env/prouction.js
@@ -0,0 +1,7 @@
+const prodConfig = {
+  env: 'production',
+  db: 'mongodb://mongo/api_server',
+  port: 4000
+}
+
+module.exports = prodConfig
