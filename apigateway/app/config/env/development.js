0fdeaa83e3441832a11e1a209e849ffda2991a0c
Samuel Masuy <smasuy@jive.com>
2017-02-16 20:25:56 -0500

    [apigateway] db url optionnally in env variable

--- apigateway/app/config/env/development.js
+++ apigateway/app/config/env/development.js
@@ -1,8 +1,9 @@
 const devConfig = {
   env: 'development',
   MONGOOSE_DEBUG: true,
-  db: 'mongodb://localhost/api_server',
+  db: process.env.DB || 'mongodb://localhost/api_server',
   port: 4000
 }
 
 module.exports = devConfig
+
