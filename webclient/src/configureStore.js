c1962fbbde8e22f562a1ec5a0e9c62aece7a8d88
Samuel Masuy <smasuy@jive.com>
2017-03-17 15:36:45 -0400

    [webclient] split dev from prod, use redux-DevTools

--- webclient/src/configureStore.js
+++ webclient/src/configureStore.js
@@ -2,25 +2,34 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import { createEpicMiddleware } from 'redux-observable'
 import { routerMiddleware } from 'react-router-redux'
 import { browserHistory } from 'react-router'
-import logger from 'redux-logger'
 import thunk from 'redux-thunk'
+import DevTools from './containers/DevTools'
+
 import rootReducer from './reducers'
 import rootEpic from './epics'
 
 const epicMiddleware = createEpicMiddleware(rootEpic)
 
-export default function configureStore () {
+export default function configureStore (initialState) {
   const routingMiddleware = routerMiddleware(browserHistory)
-  const store = createStore(
-    rootReducer,
-    compose(
-      applyMiddleware(
-        logger(),
-        epicMiddleware,
-        thunk,
-        routingMiddleware
+  let enhancer = {}
+  if (process.env.NODE_ENV === 'production') {
+    enhancer = compose(
+        applyMiddleware(
+          epicMiddleware,
+          thunk,
+          routingMiddleware
+        )
       )
-    )
+  } else {
+    enhancer = compose(
+        applyMiddleware(epicMiddleware, thunk, routingMiddleware),
+        DevTools.instrument()
+      )
+  }
+  return createStore(
+    rootReducer,
+    initialState,
+    enhancer
   )
-  return store
 }
