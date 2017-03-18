bfcaf33dd01207d408622d12fd42f5f373913d82
tunguyennnnn <conghoa522@gmail.com>
2017-03-18 00:49:01 -0400

    fixed updating user bookmark and history

--- apigateway/app/server/controllers/user.controller.js
+++ apigateway/app/server/controllers/user.controller.js
@@ -35,66 +35,31 @@ function userProfile (req, res, next) {
 }
 
 function userProfileUpdate (req, res, next) {
-  res.status(httpStatus.NO_CONTENT).send()
-// //TODO only update bookmark, favs and history
-//   let userId = req.params.user_id
-//   if (mongoose.Types.ObjectId.isValid(userId)) {
-//     let token = req.body.token || req.query.token || req.headers['x-access-token']
-//     let {email = '', password = ''} = req.body
-//     if (token) {
-//       jwt.verify(token, 'tusamjavier', function (err, decoded) {
-//         if (err) {
-//           res.status(401).send({success: false, message: 'wrong token!'})
-//         } else {
-//           email = decoded._doc.email
-//           password = decoded._doc.password
-//         }
-//       })
-//     }
-//     User.findById(userId)
-//       .then((user) => {
-//         if (email === user.email && password === user.password) {
-//           if (req.body.firstName || req.body.lastName || req.body.newPassword) {
-//             let {firstName = user.firstName, lastName = user.lastName, newPassword = user.password} = req.body
-//             user.firstName = firstName
-//             user.lastName = lastName
-//             user.password = newPassword
-//             user.save()
-//                 .then((user) => {
-//                   res.status(200).json(user)
-//                 })
-//           } else {
-//             if (req.body.history) {
-//               User.update({_id: user._id, 'history.videoId': {$ne: req.body.history}},
-//                 {$push: {history: {$each: [{videoId: req.body.history}], $slice: -10}}}
-//               )
-//               .then((user) => {
-//                 res.json(user)
-//               })
-//               .catch((err) => {
-//                 res.status(400).json({success: false, message: err})
-//               })
-//             } else if (req.body.bookmark) {
-//               User.update({_id: user._id, 'bookmark.videoId': {$ne: req.body.bookmark}},
-//                 {$push: {bookmark: {videoId: req.body.bookmark}}}
-//               )
-//               .then((user) => {
-//                 res.json(user)
-//               })
-//               .catch(err => { res.status(400).json(err) })
-//             }
-//           }
-//         } else {
-//           res.status(404)
-//           res.json({success: false, message: 'Incorrect email or password'})
-//         }
-//       })
-//       .catch(function (err) {
-//         res.json(err)
-//       })
-//   } else {
-//     res.json('dasdads')
-//   }
+  const {sub} = req.user;
+  if (sub){
+    User.findOne({userId: sub})
+      .then(function(user){
+        if (res.body.history){
+          return User.update({_id: user._id, 'history.videoId': {$ne: req.body.history}},
+                {$push: {history: {$each: [{videoId: req.body.history}], $slice: -10}}}
+              )
+        }
+        else if (res.body.bookmark){
+          return User.update({_id: user._id, 'bookmark.videoId': {$ne: req.body.bookmark}},
+                      {$push: {bookmark: {videoId: req.body.bookmark}}}
+          )
+        }
+      })
+      .then(function(user){
+        res.json(200).json(user)
+      })
+      .catch(function(err){
+        res.json(404).send();
+      });
+  }
+  else{
+    res.json(400).send();
+  }
 }
 
 module.exports = { userProfile, userProfileUpdate }
