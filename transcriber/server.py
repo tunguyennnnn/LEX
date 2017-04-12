4fbd6aeffe2f3c928ed5b2c7d0efb405d75f09c9
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2017-04-11 20:55:26 -0400

    Implemented simplistic API for Transcriber

--- transcriber/server.py
+++ transcriber/server.py
@@ -1,9 +1,11 @@
 # This is the main entry point for the webserver that implements the basic REST API
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from pymongo import MongoClient
 
 app = Flask("Transcriber")
+
+fake_list = []
 
 @app.route('/videos', methods=['POST','PUT'])
 def put_video():
@@ -12,7 +14,15 @@ def put_video():
 	Use this by calling 'POST /videos'
 	with a JSON object that has a 'video_url' key pointing to a list of URLs
 	"""
-	raise NotImplementedError("Missing REST API implementation")
+	global fake_list # TODO: replace with actual mongodb connection
+	if not request.json or not 'video_url' in request.json:
+		abort(400)
+	
+	video_list = request.json['video_url']
+	print("Received {}".format(video_list))
+	fake_list.extend(video_list)
+	
+	return jsonify({'new_videos':video_list}), 200
 #end put_video
 	
 @app.route('/videos', methods=['GET'])
@@ -22,7 +32,8 @@ def get_video():
 	Use this by calling 'GET /videos'
 	Returns a JSON object with the full list of URLs waiting to be processed
 	"""
-	raise NotImplementedError("Missing REST API implementation")
+	global fake_list # TODO: replace with actual mongodb connection
+	return jsonify({'queued_videos':fake_list}), 200
 #end get_video
 
 def run_server(args=None):
