c47c570195fb6702d343a49fa30510706256847a
Samuel Masuy <smasuy@jive.com>
2017-04-26 23:00:13 -0400

    [infra] Deployment for queue

--- infra/create-all-pods.sh
+++
@@ -1,5 +0,0 @@
-kubectl create -f mongo.yaml
-kubectl create -f transcriber.yaml
-kubectl create -f apigateway.yaml
-kubectl create -f webclient.yaml
-
--- infra/delete-all-pods.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete -f mongo.yaml
-kubectl delete -f transcriber.yaml
-kubectl delete -f apigateway.yaml
-kubectl delete -f webclient.yaml
-
